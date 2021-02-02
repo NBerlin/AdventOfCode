ca6572146b807aa14c550b41255e423a5823479a
Nicki Berlin <nickiberlinpolhem@gmail.com>
2021-02-02 12:55:53 +0100

    A little less shit

--- 2020/1.py
+++ 2020/1.py
@@ -1,15 +1,18 @@
-#part1
 file = open("input1")
 inputs = file.read().splitlines()
 inputs = [int(i) for i in inputs]
-for x in inputs:
-    for y in inputs:
-        if x+y == 2020:
-            print(x*y)
 
-#part2
-for x in inputs:
-    for y in inputs:
-        for z in inputs:
-            if x+y+z == 2020:
-                print(x*y*z)
+def part1():
+    for x in inputs:
+        for y in inputs:
+            if x+y == 2020:
+                return x*y
+
+def part2():
+    for x in inputs:
+        for y in inputs:
+            for z in inputs:
+                if x+y+z == 2020:
+                    return x*y*z
+
+print(part1(),part2())
